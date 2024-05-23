69299b79305f5a6eb9f42fbb3f5b6bf02733bf46
reo0105 <skreo0105@gmail.com>
2024-05-23 10:16:57 +0900

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,57 @@
+  
+document.getElementById("addTaskButton").addEventListener("click", function() {
+    const taskInput = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskLists");
+
+    const taskText = taskInput.value.trim();
+    if (taskText === "")  return;
+
+    const taskItem = document.createElement("li");
+
+    const p = document.createElement("p");
+    p.innerHTML = taskText;
+
+    const endButton = document.createElement("button");
+    endButton.setAttribute('class', "endButton");
+    endButton.innerHTML = "done";
+    endButton.value = "Not Done";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.setAttribute("class", "deleteButton");
+    deleteButton.innerHTML = "delete";
+
+    taskItem.appendChild(p);
+    taskItem.appendChild(endButton);
+    taskItem.appendChild(deleteButton);
+
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+
+    endButton.addEventListener("click", function(e) {
+        // e.preventDefault();
+        if (endButton.value === "Not Done") {
+            this.setAttribute("class", "endButtonDisable");
+            this.previousElementSibling.setAttribute('class', 'doneToDo');
+            endButton.value = "Done";
+        } else {
+            this.setAttribute("class", "endButton");
+            this.previousElementSibling.setAttribute('class', 'notDoneTodo');
+            endButton.value = "Not Done";
+        }
+    });
+    
+    deleteButton.addEventListener("click", function(e) {
+        taskList.removeChild(this.closest("li"));
+    });
+
+});
+
+document.getElementById("taskInput").addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        document.getElementById("addTaskButton").click();
+    }
+});
+  
+  
+  
